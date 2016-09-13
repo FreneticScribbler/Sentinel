feb499c52fd31afbdaceecc7fbb64bc490c7697a
Arona Jones <warlordjones@gmail.com>
2016-09-13 14:14:51 +0100

    Disconnect message

--- src/main/java/com/aronajones/sentinel/discord/DisconnectionListener.java
+++ src/main/java/com/aronajones/sentinel/discord/DisconnectionListener.java
@@ -2,12 +2,28 @@ package com.aronajones.sentinel.discord;
 
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.DiscordDisconnectedEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
 
 public class DisconnectionListener implements IListener<DiscordDisconnectedEvent> {
 
 	@Override
 	public void handle(DiscordDisconnectedEvent event) {
-		System.out.println("disconnected");
+		for(IGuild guild : event.getClient().getGuilds()) {
+
+			IChannel main = guild.getChannels().get(0); // TODO
+
+			try {
+				main.sendMessage("Sentinel deactivating...");
+			}
+			catch(MissingPermissionsException | RateLimitException | DiscordException e) {
+				e.printStackTrace();
+			}
+		}
+
 		StorageHandler.writeDataToDisk();
 	}
 
